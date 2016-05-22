fd3ba56739e1131983806d86adacb424446f0791
houtsandrew <houtsandrew@gmail.com>
2016-05-22 10:10:33 -0700

    worked on the H file for Change

--- CIS22C_FinalProject/CIS22C_FinalProject/Change.h
+++ CIS22C_FinalProject/CIS22C_FinalProject/Change.h
@@ -1,27 +1,32 @@
-#include <iostream>
 
+#ifndef CHANGE_H
+#define CHANGE_H
+
+#include <string>
+#include "Sheet.h"
 using namespace std;
 
 class Change
 {
 private:
-	Change prev;
-	Change next;
+	Change *prev;
+	Change *next;
 	int row;
 	int col;
-	cell prevData;
-	cell newData;
+	string prevData;
+	string newData;
 	bool isOpenParen;
 	bool isCloseParen;
+	bool isHead;		// if it is the head Change, don't undo it
 public:
-	
-	Change(Change prev, Change next, int row, int col, cell prevData, cell newData);
-	void undo();
-	void pushBack(int row, int col, cell prevData, cell newData);
-	Change(bool isOpenParen, bool iscloseParen);
+	// This creates the head Change that will be used in other classes
+	Change();
+	// true = openParen, false = closeParen
+	Change(bool openOrClose);
+	Change(int row, int col, string prevData, string newData, Change *prev, Change *next = nullptr);
+	void undo(Sheet *sheet);
+	// we assume that this will only be called on the head Change
+	Change* pushBack(int row, int col, string prevData, string newData);
+};
 
-
-
-
-
-}
+#endif
