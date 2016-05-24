a8f29707c0026235f08b78696f1113114eba2f28
whal3man <lucke97@gmail.com>
2016-05-24 16:44:16 -0700

    made the refresh.h file

---
+++ CIS22C_FinalProject/CIS22C_FinalProject/Refresh.h
@@ -0,0 +1,29 @@
+#ifndef REFRESH_H
+#define REFRESH_H
+#include "Sheet.h"
+
+class Refresh
+{
+private:
+	Sheet newsheet;
+public:
+	Refresh::Refresh();
+	void findfunctions();
+	double add(int x1, int x2, int y1, int y2);
+	double subtract(int x1, int x2, int y1, int y2);
+	double multiply(int x1, int x2, int y1, int y2);
+	double divide(int x1, int x2, int y1, int y2);
+	void sortRow(int x1, int x2);
+	void sortCol(y1, y2);
+	double average(int x1, int x2, int y1, int y2);
+	
+
+
+
+
+
+
+
+};
+
+#endif	 REFRESH_H
