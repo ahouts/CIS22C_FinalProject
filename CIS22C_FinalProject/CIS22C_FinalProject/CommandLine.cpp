52d8b7f4a221c0bbc3c58743c9fbab6c692c86bc
whal3man <lucke97@gmail.com>
2016-06-02 17:31:54 -0700

    worked on command line

--- CIS22C_FinalProject/CIS22C_FinalProject/CommandLine.cpp
+++ CIS22C_FinalProject/CIS22C_FinalProject/CommandLine.cpp
@@ -1,19 +1,54 @@
 #include "CommandLine.h"
-#include "Sheet.h"
-#include "Refresh.h"
-#include "Cell.h"
-#include <string>
+
 
 void CommandLine::modifyCell(int xCoord, int yCoord, string data)
 {
-
-	Cell.setData(data);
+	sheet->setCellData(xCoord, yCoord, data);
 }
-void CommandLine::drawSheet(ostream& out) //draws the sheet 
+void CommandLine::drawSheet(ostream& out) 
 {
-
+	for (int x = 0; x < sheet->getXSize; x++)
+	{
+		for (int y = 0; y < sheet->getYSize; y++)
+		{
+			out << sheet->getCellData(x, y);
+		}
+	}
 }
-void CommandLine::mainLoop() // calls main menu
+void CommandLine::mainLoop()
 {
+	
+	drawSheet(cout);
+	cout << "enter the command you wish to do next: ";
+	string masterString;
+	getline(cin, masterString);
+	
+	string masterString("hello world this is andrew speaking");
 
+
+	stringstream iss(masterString);
+
+	while (iss)
+	{
+		string word[100];
+		for (int i = 0; i < masterString.length(); i++)
+		{
+			iss >> word[i];
+		}
+
+		// || SET VALUE ||
+		if (word[0] == "set")
+		{
+			if (stoi(word[1]))
+			{
+				int a = stoi(word[1]);
+				if (stoi(word[2]))
+				{
+					int b = stoi(word[2]);
+					modifyCell(a, b, word[3]);
+				}
+			}
+		}
+		
+	}
 }
