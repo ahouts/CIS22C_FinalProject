4f03f507ae666cc50cba9806b8338291b60fb4a1
roarkburney <roark.burney@me.com>
2016-06-12 14:36:03 -0700

    changes to findfunctions()

--- CIS22C_FinalProject/CIS22C_FinalProject/Refresh.cpp
+++ CIS22C_FinalProject/CIS22C_FinalProject/Refresh.cpp
@@ -114,10 +114,14 @@ Refresh::Refresh(Sheet *sheet)
 void Refresh::findfunctions()
 {
 	string calc = getHead().getMe();
-	int goThrough = 0;
-	while (calc[goThrough] != '+' || calc[goThrough] != '-' || calc[goThrough] != '/' || calc[goThrough] != '*') {
+	/*char * numbers = new char[calc.length() + 1];
+	std::strcpy(numbers, calc.c_str());
 
-	}
+	char * numberArray = std::strtok(numbers, "-" || "+" || "/" || "/"||"("||")");
+	char * operators = new char[calc.length() + 1];
+	std::strcpy(operators, calc.c_str());
+	char */
+
 }
 
 double Refresh::add(int xCoord, int yCoord, int xCoord1, int yCoord1)
