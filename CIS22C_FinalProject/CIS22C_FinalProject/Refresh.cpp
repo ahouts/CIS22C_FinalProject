4eaafd0e3430819a444a5bb7f76347d41475a33b
roarkburney <roark.burney@me.com>
2016-06-12 15:07:03 -0700

    fixed memory leak

--- CIS22C_FinalProject/CIS22C_FinalProject/Refresh.cpp
+++ CIS22C_FinalProject/CIS22C_FinalProject/Refresh.cpp
@@ -1,5 +1,5 @@
 #include "Refresh.h"
-
+#include "BST.h"
 double Refresh::average(int xCoord, int yCoord, int xCoord1, int yCoord1)
 {
 	int itterations = 0;
@@ -113,14 +113,6 @@ Refresh::Refresh(Sheet *sheet)
 
 void Refresh::findfunctions()
 {
-	string calc = getHead().getMe();
-	/*char * numbers = new char[calc.length() + 1];
-	std::strcpy(numbers, calc.c_str());
-
-	char * numberArray = std::strtok(numbers, "-" || "+" || "/" || "/"||"("||")");
-	char * operators = new char[calc.length() + 1];
-	std::strcpy(operators, calc.c_str());
-	char */
 
 }
 
