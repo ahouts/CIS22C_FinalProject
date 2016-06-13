5a2edfdce293cf85a5f098c0296b891415596882
roarkburney <roark.burney@me.com>
2016-06-12 17:41:09 -0700

    backing up to server

--- CIS22C_FinalProject/CIS22C_FinalProject/Refresh.h
+++ CIS22C_FinalProject/CIS22C_FinalProject/Refresh.h
@@ -10,13 +10,13 @@ private:
 public:
 	Refresh(Sheet *sheet);
 	void findfunctions();
-	double add(int x1, int x2, int y1, int y2);
-	double subtract(int x1, int x2, int y1, int y2);
-	double multiply(int x1, int x2, int y1, int y2);
-	double divide(int x1, int x2, int y1, int y2);
-	void sortRow(int x1, int x2);
-	void sortCol(int y1, int y2);
-	double average(int x1, int x2, int y1, int y2);
+	double add(int xCoord, int yCoord, int xCoord1, int yCoord1);
+	double subtract(int xCoord, int yCoord, int xCoord1, int yCoord1);
+	double multiply(int xCoord, int yCoord, int xCoord1, int yCoord1);
+	double divide(int xCoord, int yCoord, int xCoord1, int yCoord1);
+	void sortRow(int x1, int x2, int y);
+	void sortCol(int y1, int y2, int x);
+	double average(int xCoord, int yCoord, int xCoord1, int yCoord1);
 };
 
 #endif
