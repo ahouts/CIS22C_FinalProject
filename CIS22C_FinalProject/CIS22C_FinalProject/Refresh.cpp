1975279b46d3a7af2e59ae7ee9df6b58bfa149a9
Andrew Houts <tester.556539@yahoo.com>
2016-06-13 19:34:58 -0700

    roarks refresh changes

--- CIS22C_FinalProject/CIS22C_FinalProject/Refresh.cpp
+++ CIS22C_FinalProject/CIS22C_FinalProject/Refresh.cpp
@@ -1,6 +1,5 @@
 #include "Refresh.h"
 
-
 double Refresh::average(int xCoord, int yCoord, int xCoord1, int yCoord1)
 {
 	int itterations = 0;
@@ -115,13 +114,39 @@ Refresh::Refresh(Sheet *sheet)
 void Refresh::findfunctions()
 {
 
+	for (int i = 0; i < sheet->getXSize(); i++) {
+		for (int p = 0; p < sheet->getYSize(); p++) {
+			string *answer = new string[5];
+			for (int i = 0; i < 5; i++)
+			{
+				answer[i] = "";
+			}
+			string line = sheet->getCellData(i, p);
+			stringstream ssin(line);
+			int count = 0;
+			while (ssin.good() && count < 5)
+			{
+				ssin >> answer[count];
+				count++;
+			}
+			if (answer[0] == "add") {
+				sheet->setCellData(i, p, to_string(add(stoi(answer[1]), stoi(answer[2]), stoi(answer[3]), stoi(answer[4]))));
+			}
+			if (answer[0] == "multiply") {
+				sheet->setCellData(i, p, to_string(multiply(stoi(answer[1]), stoi(answer[2]), stoi(answer[3]), stoi(answer[4]))));
+			}
+			if (answer[0] == "average") {
+				sheet->setCellData(i, p, to_string(average(stoi(answer[1]), stoi(answer[2]), stoi(answer[3]), stoi(answer[4]))));
+			}
+		}
+	}
 }
 
 double Refresh::add(int xCoord, int yCoord, int xCoord1, int yCoord1)
 {
 	int a = stoi(sheet->getCellData(xCoord, yCoord));
 	int b = stoi(sheet->getCellData(xCoord1, yCoord1));
-	double c = a+b;
+	double c = a + b;
 	return c;
 }
 
@@ -140,9 +165,40 @@ double Refresh::subtract(int xCoord, int yCoord, int xCoord1, int yCoord1)
 	double c = a - b;
 	return c;
 }
+
 double Refresh::divide(int xCoord, int yCoord, int xCoord1, int yCoord1) {
 	double a = stoi(sheet->getCellData(xCoord, yCoord));
 	double b = stoi(sheet->getCellData(xCoord1, yCoord1));
 	double c = a / b;
 	return c;
+}
+
+void Refresh::sortRow(int x1, int x2, int y) {
+	string* sorter;
+	sorter = new string[x2 - x1];
+	for (int i = 0; i < (x2 - x1); i++) {
+		sorter[i] = sheet->getCellData(i, y);
+	}
+	for (int i = 0; i < (x2 - x1); i++) {
+		for (int p = 0; p < i; p++) {
+			if (sorter[i] > sorter[p]) {
+				sheet->swapCol(i, p);
+			}
+		}
+	}
+}
+
+void Refresh::sortCol(int y1, int y2, int x) {
+	string* sorter;
+	sorter = new string[y2 - y1];
+	for (int i = 0; i < (y2 - y1); i++) {
+		sorter[i] = sheet->getCellData(i, x);
+	}
+	for (int i = 0; i < (y2 - y1); i++) {
+		for (int p = 0; p < i; p++) {
+			if (sorter[i] > sorter[p]) {
+				sheet->swapRow(i, p);
+			}
+		}
+	}
 }
--- CIS22C_FinalProject/CIS22C_FinalProject/Refresh.h
+++ CIS22C_FinalProject/CIS22C_FinalProject/Refresh.h
@@ -14,8 +14,8 @@ public:
 	double subtract(int x1, int x2, int y1, int y2);
 	double multiply(int x1, int x2, int y1, int y2);
 	double divide(int x1, int x2, int y1, int y2);
-	void sortRow(int x1, int x2);
-	void sortCol(int y1, int y2);
+	void sortRow(int x1, int x2, int y);
+	void sortCol(int y1, int y2, int x);
 	double average(int x1, int x2, int y1, int y2);
 };
 
